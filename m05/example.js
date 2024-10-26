07a5eebb849701e4b7cb77bdeada349ffbeb593f
youngseodaisyyi <146045253+youngseodaisyyi@users.noreply.github.com>
2024-10-25 18:40:41 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -1,20 +1,17 @@
-prompt("Input a number between 0-10 to see the multiplication table for the desired number.", "0-10"); // Prompt User
+var input = prompt("Input a number between 0-10 to see the multiplication table for the desired number.", "0-10"); // Prompt User
+var num = parseInt(input); // Convert input to an integer
 var operator = 'multiplication'; // Type of calculation
 var i = 1;  // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';  // Message
 
-if (operator === 'addition') {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+ if (num >= 1 && num <= 10) {
+        var msg = '<h2>Multiplication Table for ' + number + '</h2><ul>';
+  for (var i = 1; i <= 10; i++) {
+            msg += '<li>' + number + ' x ' + i + ' = ' + (number * i) + '</li>'; // Add each calculation to the message
+        }
+        msg += '</ul>';
+   else {
+        alert("Please enter a valid number between 1 and 10.");
+    }
 }
 
 // Write the message into the page
