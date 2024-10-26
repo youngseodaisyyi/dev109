1873e281df5a36d675790416a1d21b07a24c2ab7
youngseodaisyyi <146045253+youngseodaisyyi@users.noreply.github.com>
2024-10-25 19:58:54 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -4,7 +4,7 @@ var msg = '';              // Message
 
 // Validate input
 if (num >= 1 && num <= 10) {
-        var msg = '<h2>Multiplication Table'</h2>'; // Start message
+        var msg = '<h2>Multiplication Table'</h2>'; // Message
 
 // Generate multiplication table
         for (var i = 1; i <= 10; i++) {
