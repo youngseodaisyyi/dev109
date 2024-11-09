927e313d055f5807f1ba4320ef3813d5fc5cbb66
youngseodaisyyi <146045253+youngseodaisyyi@users.noreply.github.com>
2024-11-08 16:23:31 -0800

    Update example.js

--- m07/example.js
+++ m07/example.js
@@ -12,7 +12,7 @@ function Hotel(name, rooms, booked) {
 // Create three hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-var parkHotel = new Hotel('Sunset', 86, 10);
+var sunsetHotel = new Hotel('Sunset', 86, 10);
 
 
 // Update the HTML for the page
@@ -26,8 +26,8 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
-var details3 = parkHotel.name + ' rooms: ';
-    details3 += parkHotel.checkAvailability();
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details3;
 /* 
